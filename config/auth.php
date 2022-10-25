cf18f25e98f4b1be443b4f82f24d2548d4feab54
swesweaung-scm <scm.swesweaung@gmail.com>
2022-10-25 16:22:31 +0630

    second commit

--- config/auth.php
+++ config/auth.php
@@ -34,13 +34,16 @@ return [
     | Supported: "session"
     |
     */
-
     'guards' => [
         'web' => [
             'driver' => 'session',
             'provider' => 'users',
         ],
-    ],
+        'api' => [
+            'driver' => 'passport',
+            'provider' => 'users',
+        ],
+     ],
 
     /*
     |--------------------------------------------------------------------------
